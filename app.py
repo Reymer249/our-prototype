4271bf2726f1b4d93569ea2561ac94fa9ef02fbc
Max Lytovka <litmax515@gmail.com>
2024-10-30 12:49:57 +0100

    Initial draft

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
+# Ensure upload directory exists
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files.get('file')
+        if file and file.filename.endswith('.txt'):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return render_template('upload.html')
+
+@app.route('/generate', methods=['GET', 'POST'])
+def generate():
+    if request.method == 'POST':
+        answers = {
+            'who': request.form['who'],
+            'what_problem': request.form['what_problem'],
+            'problem_description': request.form['problem_description'],
+            'time_available': request.form['time_available']
+        }
+        return redirect(url_for('guide'))
+    return render_template('generate.html')
+
+@app.route('/guide')
+def guide():
+    return render_template('guide.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
